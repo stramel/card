493abe87f1805b3b69dfab3a49e305ec3f5074e6
Michael Stramel <m.stramel89@gmail.com>
2018-12-22 09:01:12 +0000

    Updates

--- bin/card.js
+++ bin/card.js
@@ -1,55 +1,8 @@
 #!/usr/bin/env node
 
-const boxen = require('boxen')
-const chalk = require('chalk')
-const leftPad = require('left-pad')
+'use strict'
 
-const boxenOptions = {
-  padding: 1,
-  margin: 1,
-  borderStyle: 'round',
-}
-
-const newline = '\n'
-const formatLabel = value => chalk.white.bold(leftPad(value, 12) + ':')
-const formatLink = value => chalk.cyan(value)
-
-const data = [
-  {
-    label: chalk.white.bold('Michael Stramel'),
-    value: '',
-  },
-  {
-    label: '',
-    value:
-      chalk.white('Software Engineer') +
-      chalk.gray(' @ Coolfire Solutions') +
-      newline,
-  },
-  {
-    label: formatLabel('Twitter'),
-    value: formatLink('https://twitter.com/stramel89'),
-  },
-  {
-    label: formatLabel('GitHub'),
-    value: formatLink('https://github.com/stramel'),
-  },
-  {
-    label: formatLabel('LinkedIn'),
-    value: formatLink('https://linkedin.com/in/stramel'),
-  },
-  {
-    label: formatLabel('Website'),
-    value: 'COMING SOON',
-  },
-  {
-    label: newline + chalk.cyan(leftPad('$', 13)),
-    value: chalk.white('npx stramel'),
-  },
-]
-
-const output = data
-  .map(({ label, value }) => `${label}${label && ' '}${value}`)
-  .join(newline)
-
-console.log(chalk.green(boxen(output, boxenOptions)))
+const fs = require('fs')
+const path = require('path')
+const output = fs.readFileSync(path.join(__dirname, 'output'), 'utf8')
+console.log(output)
---
+++ build.js
@@ -0,0 +1,65 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const boxen = require('boxen')
+const chalk = require('chalk')
+const leftPad = require('left-pad')
+
+const boxenOptions = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'round',
+}
+
+const newline = '\n'
+const formatLabel = value => chalk.white.bold(leftPad(value, 12) + ':')
+const formatSocialLink = (link, username, color) =>
+  chalk.gray(link) + color(username)
+
+const data = [
+  {
+    label: chalk.white.bold('Michael Stramel'),
+    value: '',
+  },
+  {
+    label: '',
+    value:
+      chalk.white('Software Engineer') +
+      chalk.gray(' @ Coolfire Solutions') +
+      newline,
+  },
+  {
+    label: formatLabel('GitHub'),
+    value: formatSocialLink('https://github.com/', 'stramel', chalk.white),
+  },
+  {
+    label: formatLabel('npm'),
+    value: formatSocialLink('https://npmjs.com/~', 'stramel89', chalk.red),
+  },
+  {
+    label: formatLabel('Twitter'),
+    value: formatSocialLink('https://twitter.com/', 'stramel89', chalk.cyan),
+  },
+  {
+    label: formatLabel('LinkedIn'),
+    value: formatSocialLink('https://linkedin.com/in/', 'stramel', chalk.blue),
+  },
+  {
+    label: formatLabel('Website'),
+    value: 'COMING SOON',
+  },
+  {
+    label: newline + chalk.cyan(leftPad('$', 13)),
+    value: chalk.white('npx stramel'),
+  },
+]
+
+const output = data
+  .map(({ label, value }) => `${label}${label && ' '}${value}`)
+  .join(newline)
+
+fs.writeFileSync(
+  path.join(__dirname, 'bin/output'),
+  chalk.green(boxen(output, boxenOptions)),
+)
